42cebdaefb59d8608585bce2b307f7fcad117bd3
binbin Deng <108676127+plusbang@users.noreply.github.com>
2025-12-18 16:52:06 +0800

    add ut (#8)
    Enable pipeline and module's Unit Test.

--- samples/cpp/module_genai/load_image.cpp
+++ samples/cpp/module_genai/load_image.cpp
@@ -25,38 +25,117 @@ std::vector<ov::Tensor> utils::load_images(const std::filesystem::path& input_pa
     return {utils::load_image(input_path)};
 }
 
-ov::Tensor utils::load_image(const std::filesystem::path& image_path) {
+ov::Tensor utils::load_video(const std::filesystem::path& input_path) {
+    auto rgbs = load_images(input_path);
+    if (rgbs.size() == 0) {
+        return {};
+    }
+
+    auto video = ov::Tensor(ov::element::u8,
+                            ov::Shape{rgbs.size(), rgbs[0].get_shape()[1], rgbs[0].get_shape()[2], rgbs[0].get_shape()[3]});
+    std::cout << "video.shape = " << video.get_shape() << std::endl;
+
+    auto stride = rgbs[0].get_byte_size();
+    std::cout << "stride = " << stride << std::endl;
+    auto dst = reinterpret_cast<char*>(video.data());
+    int b = 0;
+    for (auto rgb : rgbs)
+    {
+        std::memcpy(dst + stride * b, rgb.data(), stride);
+        b++;
+    }
+    return video;
+}
+
+#include <opencv2/opencv.hpp>
+// Return video with shape: [num_frames, height, width, 3]
+ov::Tensor utils::create_countdown_frames()
+{
+    int frames_count = 5, height = 240, width = 360;
+    auto video = ov::Tensor(ov::element::u8,
+                            ov::Shape{(size_t)frames_count, (size_t)height, (size_t)width, 3});
+
+    for (int i = frames_count; i > 0; i--)
+    {
+        cv::Mat frame = cv::Mat::zeros(height, width, CV_8UC3);
+        std::string text = std::to_string(i);
+
+        int baseline = 0;
+        int fontFace = cv::FONT_HERSHEY_SIMPLEX;
+        double fontScale = 3.0; // Python '3' is a double in C++ OpenCV
+        int thickness = 4;
+
+        // The C++ getTextSize returns the size as a cv::Size
+        cv::Size textSize = cv::getTextSize(
+            text,
+            fontFace,
+            fontScale,
+            thickness,
+            &baseline // baseline is passed by pointer
+        );
+
+        int text_width = textSize.width;
+        int text_height = textSize.height;
+        int text_x = (width - text_width) / 2;
+        int text_y = (height + text_height) / 2;
+
+        cv::Scalar color = cv::Scalar(255, 255, 255); // BGR: White
+        cv::Point org(text_x, text_y);                // Origin point for the text
+
+        cv::putText(
+            frame,
+            text,
+            org,
+            fontFace,
+            fontScale,
+            color,
+            thickness,
+            cv::LINE_AA // The line type constant
+        );
+
+        int idx = frames_count - i;
+        std::memcpy((char*)video.data() + idx * height * width * 3, frame.data, height * width * 3);
+
+        // cv::imshow("Centered Text Frame", frame);
+        // cv::waitKey(0);
+    }
+    return video;
+}
+
+ov::Tensor utils::load_image(const std::filesystem::path &image_path)
+{
     int x = 0, y = 0, channels_in_file = 0;
     constexpr int desired_channels = 3;
-    unsigned char* image = stbi_load(
+    unsigned char *image = stbi_load(
         image_path.string().c_str(),
         &x, &y, &channels_in_file, desired_channels);
-    if (!image) {
+    if (!image)
+    {
         std::stringstream error_message;
         error_message << "Failed to load the image '" << image_path << "'";
         throw std::runtime_error{error_message.str()};
     }
-    struct SharedImageAllocator {
-        unsigned char* image;
+    struct SharedImageAllocator
+    {
+        unsigned char *image;
         int channels, height, width;
-        void* allocate(size_t bytes, size_t) const {
-            if (image && channels * height * width == bytes) {
+        void *allocate(size_t bytes, size_t) const
+        {
+            if (image && channels * height * width == bytes)
+            {
                 return image;
             }
             throw std::runtime_error{"Unexpected number of bytes was requested to allocate."};
         }
-        void deallocate(void*, size_t bytes, size_t) {
-            if (channels * height * width != bytes) {
-                throw std::runtime_error{"Unexpected number of bytes was requested to deallocate."};
-            }
+        void deallocate(void *, size_t, size_t) noexcept
+        {
             stbi_image_free(image);
             image = nullptr;
         }
-        bool is_equal(const SharedImageAllocator& other) const noexcept {return this == &other;}
+        bool is_equal(const SharedImageAllocator &other) const noexcept { return this == &other; }
     };
     return ov::Tensor(
         ov::element::u8,
         ov::Shape{1, size_t(y), size_t(x), size_t(desired_channels)},
-        SharedImageAllocator{image, desired_channels, y, x}
-    );
+        SharedImageAllocator{image, desired_channels, y, x});
 }
