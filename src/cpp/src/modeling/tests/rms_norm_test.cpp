b17cb2d1c0e7f25367992e8e8dd05ff35129c797
Li, Liang A <liang.a.li@intel.com>
2025-12-25 16:20:41 +0800

    run ult tests on gpu device

--- src/cpp/src/modeling/tests/rms_norm_test.cpp
+++ src/cpp/src/modeling/tests/rms_norm_test.cpp
@@ -55,8 +55,13 @@ TEST(RMSNormLayer, Basic) {
     auto result = std::make_shared<ov::op::v0::Result>(y.output());
     auto model = std::make_shared<ov::Model>(ov::OutputVector{result}, ov::ParameterVector{param});
 
+    // ov::serialize(model, "rms_ult_model_original.xml");
+
     ov::Core core;
-    auto compiled = core.compile_model(model, "CPU");
+    auto compiled = core.compile_model(model, "GPU");
+
+    // ov::serialize(compiled.get_runtime_model(), "rms_ult_model_compiled.xml");
+
     auto request = compiled.create_infer_request();
 
     std::vector<float> input = {1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f};
--- src/cpp/src/modeling/tests/tensor_ops_test.cpp
+++ src/cpp/src/modeling/tests/tensor_ops_test.cpp
@@ -43,7 +43,7 @@ TEST(TensorOps, PowMeanRsqrt) {
     auto model = std::make_shared<ov::Model>(ov::OutputVector{result}, ov::ParameterVector{param});
 
     ov::Core core;
-    auto compiled = core.compile_model(model, "CPU");
+    auto compiled = core.compile_model(model, "GPU");
     auto request = compiled.create_infer_request();
 
     std::vector<float> input = {1.f, 2.f, 3.f, 4.f, 5.f, 6.f};
