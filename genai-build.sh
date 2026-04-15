1cad0b4661189e93c7d3ad050f5c2f41111c1bd3
Zhang, Xiaolin <xiaolin.zhang@intel.com>
2026-04-15 10:33:17 +0800

    update script and tokenizer version
    Signed-off-by: Zhang, Xiaolin <xiaolin.zhang@intel.com>

--- genai-build.sh
+++ genai-build.sh
@@ -1,5 +1,5 @@
 mkdir -p build && cd build
-OV_DIR=/media/xzhan34/data/zlab_dflash_ws/openvino_dflash/openvino
+OV_DIR=/home/xzhan34/work/dflash_ws/openvino_dflash/openvino
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DOpenVINO_DIR="$OV_DIR/build" ..
 cmake --build . --config RelWithDebInfo --verbose -j16
 cd ..
